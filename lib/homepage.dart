68f7b0924c14a5d9a299a6b53d08c5e041016a15
Abhi Jain <devabhixda@gmail.com>
2019-10-12 18:14:02 +0530

    Add ability to count number of steps via inbuilt phone's pedometer
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:harmony/imageScanner.dart';
+import 'package:pedometer/pedometer.dart';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,27 +9,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Pedometer _pedometer;
+  StreamSubscription<int> _subscription;
+  String _stepCountValue = 'unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    startListening();
+  }
+
+  void onData(int stepCountValue) {
+    print(stepCountValue);
+  }
+
+  void startListening() {
+    _pedometer = new Pedometer();
+    _subscription = _pedometer.pedometerStream.listen(_onData,
+        onError: _onError, onDone: _onDone, cancelOnError: true);
+  }
+
+  void stopListening() {
+    _subscription.cancel();
+  }
+
+  void _onData(int stepCountValue) async {
+    setState(() => _stepCountValue = "$stepCountValue");
+  }
+
+  void _onDone() => print("Finished pedometer tracking");
+
+  void _onError(error) => print("Flutter Pedometer Error: $error");
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Harmony Health"),
+        appBar: new AppBar(
+          title: const Text('Harmony Health'),
+        ),
+        body: new Text('Step count: $_stepCountValue'),
+        floatingActionButton: new FloatingActionButton(
+          child: new Icon(Icons.scanner),
+          onPressed: () {
+            Navigator.push(context,
+            MaterialPageRoute(builder: (context) => Scanner()));
+          },
+        ),
       ),
-        body:  Container(
-          width: 100,
-          height: 100,
-          child: OutlineButton(
-            child: Text("Click here"),
-            onPressed: () => {
-              Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Scanner()))
-
-            }
-            ),
-        )
-      )
     );
   }
 }
